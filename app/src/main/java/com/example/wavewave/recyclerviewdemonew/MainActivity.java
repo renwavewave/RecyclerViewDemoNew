c51293134a0cd8744c42e0e6704d6f0833d7eb40
wavewave <wavewave521@163.com>
2019-03-08 18:51:43 +0800

    案例完成

--- app/src/main/java/com/example/wavewave/recyclerviewdemonew/MainActivity.java
+++ app/src/main/java/com/example/wavewave/recyclerviewdemonew/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.wavewave.recyclerviewdemonew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        LinkedList<String> linkedList = new LinkedList();
+        for (int i = 0; i < 20; i++)
+            linkedList.add("小明" + i + "号1");
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        //配置布局，默认为vertical（垂直布局），下边这句将布局改为水平布局
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(new TextAdapter(linkedList));
+
     }
 }
---
+++ app/src/main/java/com/example/wavewave/recyclerviewdemonew/TextAdapter.java
@@ -0,0 +1,59 @@
+package com.example.wavewave.recyclerviewdemonew;
+
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+public class TextAdapter extends RecyclerView.Adapter<TextAdapter.TextViewHolder> {
+    private LinkedList<String> list;
+    private TextView textView;
+
+    public TextAdapter(LinkedList<String> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public TextViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new TextViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.my_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final TextViewHolder holder, int position) {
+        holder.textView.setText(list.get(position) + "");
+        if (position == 0) {
+            textView = holder.textView;
+            holder.textView.setTextColor(Color.parseColor("#FF4081"));
+        }
+        holder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textView != null) {
+                    textView.setTextColor(Color.parseColor("#000000"));
+                }
+                holder.textView.setTextColor(Color.parseColor("#FF4081"));
+                textView = holder.textView;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    class TextViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public TextViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
